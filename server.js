08aee0dd9b54dbce757fde7d99743d15d4d016c6
Paul McKenna <user1@user1s-mbp.home>
2020-06-01 16:41:04 +0100

    start from server

--- server.js
+++ server.js
@@ -2,19 +2,34 @@ const express = require('express');
 const next = require('next');
 const path = require('path');
 const bodyParser = require('body-parser');
+const compression = require('compression');
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dir: '.', dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = express();
-    // Static files
-    // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+
+    server.use(compression());
+
+    // server.use(function(req, res, next) {
+    //     if(req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+    //       const secureUrl = "https://" + req.headers['host'] + req.url; 
+    //       res.writeHead(301, { "Location":  secureUrl });
+    //       res.end();
+    //     }
+    //     next();
+    // });   
+
+    server.get('/sitemap.xml', (req, res) => {
+        const sitemap = path.join(__dirname, 'public', 'sitemap.xml')
+        app.serveStatic(req, res, sitemap)
+    })
+
     server.use('/images', express.static(path.join(__dirname, 'images'), {
         maxAge: dev ? '0' : '365d'
     }));
-
-    server.use(bodyParser.json());
 
     server.get('*', (req, res) => {
         return handle(req, res)

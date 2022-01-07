4a555c0db748537ca5f90674733738e505c98d1d
Paul McKenna <mckennapaul27@gmail.com>
2022-01-07 17:50:39 +0000

    chjanged price

--- pages/one-to-one-therapy.js
+++ pages/one-to-one-therapy.js
@@ -1,35 +1,74 @@
-import React, { Component } from "react";
-import NavbarTwo from "../components/NavbarTwo";
-import PageBanner from "../components/PageBanner";
-import Footer from "../components/Footer";
-import CalmingBlue from "../images/calming-blue.jpg";
+import React, { Component } from 'react';
+import NavbarTwo from '../components/NavbarTwo';
+import PageBanner from '../components/PageBanner';
+import Footer from '../components/Footer';
+import CalmingBlue from '../images/calming-blue.jpg';
 import Link from 'next/link';
 
 export default class OneToOne extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavbarTwo />
-        <PageBanner pageTitle="One-to-One Therapy" pageImg={CalmingBlue} />
-        <div className="text-container ptb-100">
-          <div className="container">
-                <h3>One-to-One Therapy </h3>
-                <p>One to one therapy usually takes place with your therapist once a week or every fortnight. If CBT is recommended a typical course of treatment usually lasts for between 8 and 20 sessions, with each session lasting 50 minutes.</p>
-                <p>Therapy starts with an in-depth assessment which is usually spread over one or two sessions, this allows a shared understanding to be built with your therapist of the development and maintenance of your problem. During the sessions, you will work collaboratively with your therapist to break down your problems into their separate parts; such as your thoughts, physical symptoms, feelings and actions/behaviours. With your therapist you will analyse these areas to work out if they are unrealistic or unhelpful and to determine the effect they have on each other and you. Your therapist will then be able to help you work out how to change unhelpful thoughts and behaviours.</p>
-                <p>After working out what you can change, your therapist will help you to learn skills and techniques which you will practice during the session and also between sessions as a home task. This will then be discussed how you got on with the task at the next session. &nbsp;</p>
-                <p>The aim of therapy is to teach you to become your own therapist, allowing you to continue to apply the skills which you have learnt during treatment after your course of treatment ends.</p>
-                <p><strong>All one-to-one therapy sessions are priced at &pound;60 per session, payable at each session.</strong></p>
-                <Link href='/contact'>
-                    <a className='btn btn-primary'>
-                        Contact Me
-                    </a>
-                </Link>
-          </div>
-        </div>
+    render() {
+        return (
+            <React.Fragment>
+                <NavbarTwo />
+                <PageBanner
+                    pageTitle='One-to-One Therapy'
+                    pageImg={CalmingBlue}
+                />
+                <div className='text-container ptb-100'>
+                    <div className='container'>
+                        <h3>One-to-One Therapy </h3>
+                        <p>
+                            One to one therapy usually takes place with your
+                            therapist once a week or every fortnight. If CBT is
+                            recommended a typical course of treatment usually
+                            lasts for between 8 and 20 sessions, with each
+                            session lasting 50 minutes.
+                        </p>
+                        <p>
+                            Therapy starts with an in-depth assessment which is
+                            usually spread over one or two sessions, this allows
+                            a shared understanding to be built with your
+                            therapist of the development and maintenance of your
+                            problem. During the sessions, you will work
+                            collaboratively with your therapist to break down
+                            your problems into their separate parts; such as
+                            your thoughts, physical symptoms, feelings and
+                            actions/behaviours. With your therapist you will
+                            analyse these areas to work out if they are
+                            unrealistic or unhelpful and to determine the effect
+                            they have on each other and you. Your therapist will
+                            then be able to help you work out how to change
+                            unhelpful thoughts and behaviours.
+                        </p>
+                        <p>
+                            After working out what you can change, your
+                            therapist will help you to learn skills and
+                            techniques which you will practice during the
+                            session and also between sessions as a home task.
+                            This will then be discussed how you got on with the
+                            task at the next session. &nbsp;
+                        </p>
+                        <p>
+                            The aim of therapy is to teach you to become your
+                            own therapist, allowing you to continue to apply the
+                            skills which you have learnt during treatment after
+                            your course of treatment ends.
+                        </p>
+                        <p>
+                            <strong>
+                                All one-to-one therapy sessions are priced at
+                                &pound;75 per 50 minute session, payable at each
+                                session.
+                            </strong>
+                        </p>
+                        <Link href='/contact'>
+                            <a className='btn btn-primary'>Contact Me</a>
+                        </Link>
+                    </div>
+                </div>
 
-        <Footer />
-      </React.Fragment>
-    );
-  }
+                <Footer />
+            </React.Fragment>
+        );
+    }
 }
-
--- pages/online-therapy.js
+++ pages/online-therapy.js
@@ -1,37 +1,61 @@
-import React, { Component } from "react";
-import NavbarTwo from "../components/NavbarTwo";
-import PageBanner from "../components/PageBanner";
-import Footer from "../components/Footer";
-import CalmingBlue from "../images/calming-blue.jpg";
+import React, { Component } from 'react';
+import NavbarTwo from '../components/NavbarTwo';
+import PageBanner from '../components/PageBanner';
+import Footer from '../components/Footer';
+import CalmingBlue from '../images/calming-blue.jpg';
 import Link from 'next/link';
 
 export default class OnlineTherapy extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavbarTwo />
-        <PageBanner pageTitle="Online Therapy" pageImg={CalmingBlue} />
-        <div className="text-container ptb-100">
-          <div className="container">
-            <h3>Online Therapy</h3>
-            <p>You can now choose to have your therapy online rather than a face to face basis if you need more flexibility with appointments.</p>
-            
-            <p>Privacy and security are taken very seriously, therefore an online telemedicine platform called Doxy.me is used to deliver therapy online. Doxy.me has been chosen as it is more secure than some of the other online platforms, and it also has less connection problems. Doxy.me implements state of the art security and encryption protocols to assure that data integrity and privacy is maintained. Doxy.me complies with HIPPA and HITECH requirements.</p>
-            
-            <p>You are not required to register for an account with Doxy.me, we will send you the link to access the virtual waiting room at the time of your appointment and Kelly-Anne will connect with you from there. All you need to do is ensure your computer or smartphone that you are calling from has a stable internet connection, a webcam and a microphone installed.</p>
-           
-            <p>Online therapy sessions are priced at &pound;60 per 50 minute session. It is ideal but not essential, to meet in person for the very first CBT session before commencing online therapy.</p>
-            <Link href='/contact'>
-                <a className='btn btn-primary'>
-                    Contact Me
-                </a>
-            </Link>
-          </div>
-        </div>
+    render() {
+        return (
+            <React.Fragment>
+                <NavbarTwo />
+                <PageBanner pageTitle='Online Therapy' pageImg={CalmingBlue} />
+                <div className='text-container ptb-100'>
+                    <div className='container'>
+                        <h3>Online Therapy</h3>
+                        <p>
+                            You can now choose to have your therapy online
+                            rather than a face to face basis if you need more
+                            flexibility with appointments.
+                        </p>
 
-        <Footer />
-      </React.Fragment>
-    );
-  }
+                        <p>
+                            Privacy and security are taken very seriously,
+                            therefore an online telemedicine platform called
+                            Doxy.me is used to deliver therapy online. Doxy.me
+                            has been chosen as it is more secure than some of
+                            the other online platforms, and it also has less
+                            connection problems. Doxy.me implements state of the
+                            art security and encryption protocols to assure that
+                            data integrity and privacy is maintained. Doxy.me
+                            complies with HIPPA and HITECH requirements.
+                        </p>
+
+                        <p>
+                            You are not required to register for an account with
+                            Doxy.me, we will send you the link to access the
+                            virtual waiting room at the time of your appointment
+                            and Kelly-Anne will connect with you from there. All
+                            you need to do is ensure your computer or smartphone
+                            that you are calling from has a stable internet
+                            connection, a webcam and a microphone installed.
+                        </p>
+
+                        <p>
+                            Online therapy sessions are priced at &pound;75 per
+                            50 minute session. It is ideal but not essential, to
+                            meet in person for the very first CBT session before
+                            commencing online therapy.
+                        </p>
+                        <Link href='/contact'>
+                            <a className='btn btn-primary'>Contact Me</a>
+                        </Link>
+                    </div>
+                </div>
+
+                <Footer />
+            </React.Fragment>
+        );
+    }
 }
-

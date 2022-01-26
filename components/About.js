15cd669108eeb8f58e9b9c1f15ea51ddcc1b777a
Paul McKenna <mckennapaul27@gmail.com>
2022-01-26 16:56:14 +0000

    updated page

--- components/About.js
+++ components/About.js
@@ -4,37 +4,79 @@ import Link from 'next/link';
 class About extends Component {
     render() {
         return (
-            <section className="about-area ptb-100">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-5 col-md-12">
-                            <div className="about-image">
-                                <img src={require("../images/calming-mountains.jpg")} alt="image" className="rounded-10" />
+            <section className='about-area ptb-100'>
+                <div className='container'>
+                    <div className='row align-items-center'>
+                        <div className='col-lg-5 col-md-12'>
+                            <div className='about-image'>
+                                <img
+                                    src={require('../images/calming-mountains.jpg')}
+                                    alt='image'
+                                    className='rounded-10'
+                                />
                             </div>
                         </div>
 
-                        <div className="col-lg-7 col-md-12">
-                            <div className="about-content about-content-two">
-                                <div className="section-title">
+                        <div className='col-lg-7 col-md-12'>
+                            <div className='about-content about-content-two'>
+                                <div className='section-title'>
                                     <h2>A Warm Welcome</h2>
-                                    <p>KA-Therapy offers Cognitive Behavioural Psychotherapy, an evidence-based psychotherapy to help you improve your psychological wellbeing. Therapies are delivered in a quiet, private and comfortable environment, providing a safe therapeutic space to explore your difficulties. </p>
+                                    <p>
+                                        KA-Therapy offers Cognitive Behavioural
+                                        Psychotherapy, an evidence-based
+                                        psychotherapy to help you improve your
+                                        psychological wellbeing. Therapies are
+                                        delivered in a quiet, private and
+                                        comfortable environment, providing a
+                                        safe therapeutic space to explore your
+                                        difficulties.{' '}
+                                    </p>
                                 </div>
 
-                                <div className="about-text">
-                                   
-                                </div>
+                                <div className='about-text'></div>
 
-                                
-
-                                <div className="about-text">
+                                <div className='about-text'>
                                     <h4>My Experience</h4>
-                                    <p>I possess a warm, empathetic and supportive style which compliments my clinical expertise which I have developed over a number of years working in a variety of different roles of helping others to improve their wellbeing. I am an NMC registered Mental Health Nurse still in practice, alongside practising as a Cognitive Behavioural Psychotherapist. </p>
+                                    <p>
+                                        I possess a warm, empathetic and
+                                        supportive style which compliments my
+                                        clinical expertise which I have
+                                        developed over a number of years working
+                                        in a variety of different roles of
+                                        helping others to improve their
+                                        wellbeing. I am a BABCP registered
+                                        Cognitive Behavioural Psychotherapist
+                                        alongside being a qualified NMC
+                                        registered Mental Health Nurse. I
+                                        currently practice clinically as a
+                                        Cognitive Behavioural Psychotherapist
+                                        and work with people who experience a
+                                        range of problems. Please don't hesitate
+                                        to get in touch to see if what you are
+                                        experiencing is something which I can
+                                        help you with.
+                                    </p>
                                 </div>
-                                <div className="about-text">
-                                    <p>I have worked in a variety of different settings including; HMP Prison services, Criminal Justice, Drug & Alcohol Services, Secure Hospitals, Community Mental Health Services, Learning Disabilities, General Hospitals and Rehabilitation Centres. My passion is working with people; helping individuals to overcome their difficulties and work towards their personal goals. </p>
+                                <div className='about-text'>
+                                    <p>
+                                        I have worked in a variety of different
+                                        settings including; HMP Prison services,
+                                        Criminal Justice, Drug &amp; Alcohol
+                                        Services, Secure Hospitals, Community
+                                        Mental Health Services, Learning
+                                        Disabilities, General Hospitals and
+                                        Rehabilitation Centres. My passion is
+                                        working with people; helping individuals
+                                        to overcome their difficulties and work
+                                        towards their personal goals.
+                                    </p>
                                 </div>
-                                <div className="about-text">
-                                    <p>KA-Therapy offers services in Manchester City Centre, Stockport, Wilmslow and surrounding areas.</p>
+                                <div className='about-text'>
+                                    <p>
+                                        KA-Therapy offers services in Manchester
+                                        City Centre, Stockport, Wilmslow and
+                                        surrounding areas.
+                                    </p>
                                 </div>
                                 <Link href='/about'>
                                     <a className='btn btn-primary'>Read more</a>
--- pages/testimonials.js
+++ pages/testimonials.js
@@ -10,34 +10,219 @@ class Blog extends Component {
         return (
             <React.Fragment>
                 <NavbarTwo />
-                <PageBanner 
-                    pageTitle='Testimonials' 
-                    pageImg={CalmingBlue} 
-                />  
+                <PageBanner pageTitle='Testimonials' pageImg={CalmingBlue} />
                 {/* <BlogGridThree /> */}
-                <div className="text-container ptb-100">
-                     <div className="container">
-                        <h6>Eating Disorder. November 2020, SB.</h6>
-                        <p>Hi Kelly Anne,</p>
-                        <p>I hope you are ok...I just want to send you email to thank you so much for all the help and support you have given me within our sessions, without you I wouldn’t be getting the serious help that I really need and has possibly saved my life in all honesty...to of got that for me is something I believe you have gone well beyond and above so I couldn’t be more thankful or grateful.</p>
-                        <p>I would also like for you to forward this email on to your manager/supervisor as recognition for them to see because there should be more people like yourself out there to help and support people most in need.</p>
-                        <p>Once again I can’t thank you enough for everything, all the best.</p>
-                        <hr/>
-                        <h6>Hospital Phobia. December 2020, CA.</h6>
-                        <p>Hi Kelly</p>
-                        <p>Thank you for your time and commitment, your therapy helped me so much I think more positively, I am able to rationalise and challenge my thoughts and I have visited the hospital to get coffee whilst still waited in the grounds to drink this. I can’t thank you enough for your support. I no longer faint in or around hospitals or watching hospital programmes and I have not experienced a panic attack when discussing hospitals/procedures since engaging in therapy.</p>
-                        <hr/>
+                <div className='text-container ptb-100'>
+                    <div className='container'>
+                        <h6>Panic Disorder. February 2021. CW.</h6>
+                        <p>
+                            I found therapy with you to be very useful. I
+                            learned many new techniques and found it really
+                            helpful that you explained exactly why the
+                            techniques are effective and how they work.
+                        </p>
+
+                        <p>
+                            I came away from each session feeling like I’d
+                            achieved something, even if it was a very small
+                            thing. I also felt like I was learning new things
+                            constantly throughout.
+                        </p>
+
+                        <p>
+                            I really needed to be ‘pushed’ out of my comfort
+                            zone in order to do some of the tasks during the
+                            sessions, but I never felt pressured by you to do
+                            anything that I felt would be a bit too much to
+                            handle.
+                        </p>
+
+                        <p>
+                            I didn’t get to where I wanted to get to, but that’s
+                            because I wasn’t ready to move up my hierarchy, and
+                            I really appreciate that we didn’t move on to the
+                            next steps too soon because I think that would have
+                            made things worse.
+                        </p>
+
+                        <p>
+                            I’m still using techniques learned during the
+                            sessions and definitely feel it was worth giving CBT
+                            a go.
+                        </p>
+
+                        <p>
+                            Thank you so much for you time and support - I
+                            really appreciate it.
+                        </p>
+                        <hr />
+                        <h6>Social Anxiety Disorder. March 2021. PW.</h6>
+                        <p>
+                            Just like too say your session help me so much with
+                            my mental health problems. have made some goals to
+                            achieve in the future. Thank you for all your
+                            support and guidance 10/10 . I will continue to use
+                            the tools and methods you taught me and apply it to
+                            everyday living.
+                        </p>
+                        <hr />
+                        <h6>PTSD. April 2021. TM.</h6>
+                        <p>
+                            I found doing CBT with Kelly so helpful she had a
+                            natural ability to be able to make me feel relaxed
+                            and comfortable. I found challenging my thoughts
+                            feelings and behaviours so helpful although it was
+                            challenging and at times really hard. Kelly always
+                            understood and helped by bringing stories of her
+                            past experiences with either clients or herself to
+                            make it easy to understand. This also made me feel
+                            less alone and helped me to understand that others
+                            have similar feelings to me. Kelly pushed me to
+                            challenge my own thoughts and helped me to work on
+                            not avoiding things. I have come away from therapy
+                            with tools to help me to cope with life when it
+                            feels to much, to be able to face my trauma and
+                            accept it. This for me was a massive step forward
+                            for me. I really don't think I would have been able
+                            to move forward without doing CBT and having the
+                            support and help from Kelly.
+                        </p>
+                        <hr />
+                        <h6>PTSD. September 2021. LH.</h6>
+                        <p>
+                            I wanted to reflect on all the weeks that you worked
+                            with me.
+                        </p>
+                        <p>
+                            When we first met you took time to understand my
+                            situation and asked questions to ascertain where I
+                            needed support and what support I needed. Your
+                            empathy helped build a trusting relationship and
+                            created a safe space where I felt comfortable to let
+                            myself go and talk freely and honestly about my
+                            feelings.
+                        </p>
+                        <p>
+                            You came across as knowledgeable about how to
+                            support someone in my situation and provided
+                            tools/techniques for me to try. I really appreciated
+                            how you understood me and the way I approach life,
+                            which came through when you talked.
+                        </p>
+                        <p>
+                            Your questioning in our first session identified
+                            what we needed to focus on to make the most of the
+                            sessions we had together. Each week, you came across
+                            as having a clear plan on what we would discuss but
+                            also gave sufficient time and flexibility to respond
+                            to the progress I'd made during the week and any
+                            issues I wanted to go over.
+                        </p>
+                        <p>
+                            I appreciate the personal investment you had in me.
+                        </p>
+                        <p>
+                            The buzz around the return to school has hit me hard
+                            but I have confidence in my ability to allow myself
+                            to have a down day and bounce back from it by
+                            focusing on the positives and the happy memories of
+                            our family life.
+                        </p>
+                        <p>Best wishes and huge thanks</p>
+                        <hr />
+
+                        <h6>Low Self Esteem. January 2022. HF.</h6>
+                        <p>
+                            I just wanted to say a massive thanks for all the
+                            help you gave me during our sessions. In one of our
+                            first calls, we concluded that I had low self esteem
+                            and you said ‘we’ll help you improve that’. I
+                            remember feeling sceptical because it felt like such
+                            an impossible task. But down the line, you showed me
+                            that it could not only be improved – and one day I
+                            could earnestly look in the mirror and feel both
+                            love and self-compassion.
+                        </p>
+                        <p>That meant everything.</p>
+                        <hr />
                         <h6>Social Anxiety Disorder. January 2021, SH.</h6>
                         <p>Hello again Kelly,</p>
-                        <p>Just wanted reiterate my massive thank you for all your time/materials/help which is greatly appreciated. As they say “The proof is in the pudding” in our case I have come out of my shell and began to live again and not just exist as I felt I was doing before crossing paths with your kind and helpful self. The CBT you have delivered has turned my life completely around and allowed me to get two jobs and be consistently positive and confident in all life throws at me. </p>
-                        <p>I have ceased to be negative and avoid situations in fear of failure and bad outcomes instead I face things head on and ensure I am always happy and comfortable and not just being a people pleaser all the time. I have a voice now and am not afraid to express if I am not happy and have learned to change/modify things to make any experience comfortable for me and never allow others to dictate my life as I did prior to meeting you. </p>
-                        <p>THANK YOU does not seem enough to say for making me the strong person I am today. I will continue to go from strength to strength and never revisit the past negatively ever again. The techniques and mindset lessons/challenging you have taught me will be with me forever and keep me winning.</p>
+                        <p>
+                            Just wanted reiterate my massive thank you for all
+                            your time/materials/help which is greatly
+                            appreciated. As they say “The proof is in the
+                            pudding” in our case I have come out of my shell and
+                            began to live again and not just exist as I felt I
+                            was doing before crossing paths with your kind and
+                            helpful self. The CBT you have delivered has turned
+                            my life completely around and allowed me to get two
+                            jobs and be consistently positive and confident in
+                            all life throws at me.{' '}
+                        </p>
+                        <p>
+                            I have ceased to be negative and avoid situations in
+                            fear of failure and bad outcomes instead I face
+                            things head on and ensure I am always happy and
+                            comfortable and not just being a people pleaser all
+                            the time. I have a voice now and am not afraid to
+                            express if I am not happy and have learned to
+                            change/modify things to make any experience
+                            comfortable for me and never allow others to dictate
+                            my life as I did prior to meeting you.{' '}
+                        </p>
+                        <p>
+                            THANK YOU does not seem enough to say for making me
+                            the strong person I am today. I will continue to go
+                            from strength to strength and never revisit the past
+                            negatively ever again. The techniques and mindset
+                            lessons/challenging you have taught me will be with
+                            me forever and keep me winning.
+                        </p>
                         <p>Wish you and family all the best in life.</p>
                         <p>Sincerely from a success story/person.</p>
-                        <hr/>
+                        <hr />
+                        <h6>Hospital Phobia. December 2020, CA.</h6>
+                        <p>Hi Kelly</p>
+                        <p>
+                            Thank you for your time and commitment, your therapy
+                            helped me so much I think more positively, I am able
+                            to rationalise and challenge my thoughts and I have
+                            visited the hospital to get coffee whilst still
+                            waited in the grounds to drink this. I can’t thank
+                            you enough for your support. I no longer faint in or
+                            around hospitals or watching hospital programmes and
+                            I have not experienced a panic attack when
+                            discussing hospitals/procedures since engaging in
+                            therapy.
+                        </p>
+                        <hr />
+                        <h6>Eating Disorder. November 2020, SB.</h6>
+                        <p>Hi Kelly Anne,</p>
+                        <p>
+                            I hope you are ok...I just want to send you email to
+                            thank you so much for all the help and support you
+                            have given me within our sessions, without you I
+                            wouldn’t be getting the serious help that I really
+                            need and has possibly saved my life in all
+                            honesty...to of got that for me is something I
+                            believe you have gone well beyond and above so I
+                            couldn’t be more thankful or grateful.
+                        </p>
+                        <p>
+                            I would also like for you to forward this email on
+                            to your manager/supervisor as recognition for them
+                            to see because there should be more people like
+                            yourself out there to help and support people most
+                            in need.
+                        </p>
+                        <p>
+                            Once again I can’t thank you enough for everything,
+                            all the best.
+                        </p>
+                        <hr />
                     </div>
                 </div>
-                <Footer/>
+                <Footer />
             </React.Fragment>
         );
     }
